941cf95b6c3450d28ae8fc24d1d7e13c58f8303a
= <dominykasbart@gmail.com>
2026-06-02 14:51:04 +0300

    add xev tcp server

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const xev = @import("xev");
 const manager = @import("manager.zig");
+const server = @import("server.zig");
 
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
@@ -10,18 +11,21 @@ pub fn main(init: std.process.Init) !void {
     var mngr = try manager.Manager.init(gpa, init.io, size_bytes);
     defer mngr.deinit();
 
-    var loop = try xev.Loop.init(.{});
+    var tpool = xev.ThreadPool.init(.{});
+    defer tpool.deinit();
+    defer tpool.shutdown();
+
+    var loop = try xev.Loop.init(.{ .thread_pool = &tpool });
     defer loop.deinit();
 
     const w = try xev.Timer.init();
     defer w.deinit();
 
     var c: xev.Completion = undefined;
+    w.run(&loop, &c, 100, manager.Manager, &mngr, timerCallback);
 
-    w.run(&loop, &c, 100, manager.Manager, &mngr, timerCallback) catch |err| {
-        std.debug.print("Failed to start timer: {}\n", .{err});
-        return;
-    };
+    var srv = try server.Server.init(gpa, &loop, "127.0.0.1", 5882);
+    srv.start();
 
     try loop.run(.until_done);
 }
--- src/server.zig
+++ src/server.zig
@@ -1,41 +1,184 @@
 const std = @import("std");
-const net = std.net;
 const posix = std.posix;
+const xev = @import("xev");
 
-pub fn init() !void {
-    const address = try std.net.Address.parseIp("127.0.0.1", 5882);
-    const tpe: u32 = posix.SOCK.STREAM;
-    const protocol = posix.IPPROTO.TCP;
-    const listener = try posix.socket(address.any.family, tpe, protocol);
+const READ_BUF_SIZE: usize = 4096;
 
-    defer posix.close(listener);
+pub const Server = struct {
+    gpa: std.mem.Allocator,
+    loop: *xev.Loop,
+    listener: xev.TCP,
+    bound: std.Io.net.IpAddress,
+    accept_c: xev.Completion,
 
-    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
-    try posix.bind(listener, &address.any, address.getOsSockLen());
-    try posix.listen(listener, 128);
+    pub fn init(gpa: std.mem.Allocator, loop: *xev.Loop, host: []const u8, port: u16) !Server {
+        const addr = try std.Io.net.IpAddress.parse(host, port);
+        const listener = try xev.TCP.init(addr);
+        try listener.bind(addr);
+        try listener.listen(128);
 
-    var buf: [128]u8 = undefined;
-    while (true) {
-        var client_address: net.Address = undefined;
-        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
-        const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
-            std.debug.print("error accept: {}\n", .{err});
-            continue;
+        return .{
+            .gpa = gpa,
+            .loop = loop,
+            .listener = listener,
+            .bound = addr,
+            .accept_c = undefined,
         };
-        defer posix.close(socket);
+    }
 
-        std.debug.print("{} connected\n", .{client_address});
+    pub fn start(self: *Server) void {
+        std.log.info("SERVER: listening on {f}", .{self.bound});
+        self.listener.accept(self.loop, &self.accept_c, Server, self, onAccept);
+    }
 
-        const timeout = posix.timeval{ .sec = 2, .usec = 500_000 };
-        try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
-        try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.SNDTIMEO, &std.mem.toBytes(timeout));
+    fn onAccept(
+        ud: ?*Server,
+        loop: *xev.Loop,
+        _: *xev.Completion,
+        r: xev.AcceptError!xev.TCP,
+    ) xev.CallbackAction {
+        const self = ud orelse return .disarm;
 
-        const stream = std.net.Stream{ .handle = socket };
+        const client = r catch |err| {
+            std.log.err("SERVER: accept failed: {}", .{err});
+            return .rearm;
+        };
 
-        const read = try stream.read(&buf);
-        if (read == 0) {
-            continue;
+        const conn = Connection.create(self.gpa, loop, client) catch |err| {
+            std.log.err("SERVER: connection alloc failed: {}", .{err});
+            client.close(loop, &self.accept_c, void, null, dropClose);
+            return .rearm;
+        };
+        if (peerAddress(client.fd)) |peer| {
+            std.log.info("SERVER: connection accepted fd={d} peer={f}", .{ client.fd, peer });
+        } else {
+            std.log.info("SERVER: connection accepted fd={d}", .{client.fd});
         }
-        try stream.writeAll(buf[0..read]);
+        conn.beginRead();
+
+        return .rearm;
+    }
+
+    fn dropClose(
+        _: ?*void,
+        _: *xev.Loop,
+        _: *xev.Completion,
+        _: xev.TCP,
+        _: xev.CloseError!void,
+    ) xev.CallbackAction {
+        return .disarm;
+    }
+};
+
+const Connection = struct {
+    gpa: std.mem.Allocator,
+    loop: *xev.Loop,
+    socket: xev.TCP,
+    fd: posix.socket_t,
+    c: xev.Completion,
+    buf: [READ_BUF_SIZE]u8,
+
+    fn create(gpa: std.mem.Allocator, loop: *xev.Loop, socket: xev.TCP) !*Connection {
+        const conn = try gpa.create(Connection);
+        conn.* = .{
+            .gpa = gpa,
+            .loop = loop,
+            .socket = socket,
+            .fd = socket.fd,
+            .c = undefined,
+            .buf = undefined,
+        };
+        return conn;
+    }
+
+    fn beginRead(self: *Connection) void {
+        self.socket.read(self.loop, &self.c, .{ .slice = &self.buf }, Connection, self, onRead);
+    }
+
+    fn onRead(
+        ud: ?*Connection,
+        _: *xev.Loop,
+        _: *xev.Completion,
+        _: xev.TCP,
+        _: xev.ReadBuffer,
+        r: xev.ReadError!usize,
+    ) xev.CallbackAction {
+        const self = ud orelse return .disarm;
+
+        const n = r catch |err| {
+            switch (err) {
+                error.EOF => {},
+                else => std.log.err("SERVER: read failed: {}", .{err}),
+            }
+            self.beginClose();
+            return .disarm;
+        };
+
+        self.socket.write(self.loop, &self.c, .{ .slice = self.buf[0..n] }, Connection, self, onWrite);
+        return .disarm;
+    }
+
+    fn onWrite(
+        ud: ?*Connection,
+        _: *xev.Loop,
+        _: *xev.Completion,
+        _: xev.TCP,
+        _: xev.WriteBuffer,
+        r: xev.WriteError!usize,
+    ) xev.CallbackAction {
+        const self = ud orelse return .disarm;
+
+        _ = r catch |err| {
+            std.log.err("SERVER: write failed: {}", .{err});
+            self.beginClose();
+            return .disarm;
+        };
+
+        self.beginRead();
+        return .disarm;
+    }
+
+    fn beginClose(self: *Connection) void {
+        self.socket.close(self.loop, &self.c, Connection, self, onClose);
+    }
+
+    fn onClose(
+        ud: ?*Connection,
+        _: *xev.Loop,
+        _: *xev.Completion,
+        _: xev.TCP,
+        r: xev.CloseError!void,
+    ) xev.CallbackAction {
+        const self = ud orelse return .disarm;
+        _ = r catch |err| std.log.err("SERVER: close failed: {}", .{err});
+        std.log.info("SERVER: connection closed fd={d}", .{self.fd});
+        self.gpa.destroy(self);
+        return .disarm;
+    }
+};
+
+fn peerAddress(fd: posix.socket_t) ?std.Io.net.IpAddress {
+    var storage: posix.sockaddr.storage = undefined;
+    var len: posix.socklen_t = @sizeOf(@TypeOf(storage));
+    posix.getpeername(fd, @ptrCast(&storage), &len) catch return null;
+    const sa: *const posix.sockaddr = @ptrCast(&storage);
+    switch (sa.family) {
+        posix.AF.INET => {
+            const in: *const posix.sockaddr.in = @ptrCast(@alignCast(sa));
+            return .{ .ip4 = .{
+                .bytes = @bitCast(in.addr),
+                .port = std.mem.bigToNative(u16, in.port),
+            } };
+        },
+        posix.AF.INET6 => {
+            const in6: *const posix.sockaddr.in6 = @ptrCast(@alignCast(sa));
+            return .{ .ip6 = .{
+                .bytes = in6.addr,
+                .port = std.mem.bigToNative(u16, in6.port),
+                .flow = in6.flowinfo,
+                .interface = .{ .index = in6.scope_id },
+            } };
+        },
+        else => return null,
     }
 }
